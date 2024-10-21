a1a4a547aaca7d733763ccf847e51c3152a02ab3
Christiam De la Cruz <christiamdelacruz@hotmail.com>
2024-10-20 23:55:50 -0500

    correccion get nombre

--- src/main/java/com/codigo/springexamen1/service/impl/PersonaServiceImpl.java
+++ src/main/java/com/codigo/springexamen1/service/impl/PersonaServiceImpl.java
@@ -61,6 +61,7 @@ public class PersonaServiceImpl implements PersonaService {
                 .orElseThrow(() -> new NoSuchElementException("Error Cliente no existe"));
         personaActualizad.setFecha_creacion(personaObtenida.getFecha_creacion());
         if(personaActualizad.getDireccionEntity() == null) personaActualizad.setDireccionEntity(personaObtenida.getDireccionEntity());
+        if(personaActualizad.getNombre()==null )personaActualizad.setNombre(personaObtenida.getNombre());
         personaActualizad.setNum_documento(personaObtenida.getNum_documento());
         if(personaActualizad.getPedidos()==null) personaActualizad.setPedidos(personaObtenida.getPedidos());
 
